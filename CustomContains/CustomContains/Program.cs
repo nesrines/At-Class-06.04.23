5a16d8eef9c77a65d6e458c0a1fe0b6ee17e5f18
Nasrin F. Asadli <nasrinfa@code.local>
2023-04-06 15:58:00 +0400

    added 2 new algorithms

---
+++ CustomContains/CustomContains/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Add a string:");
+string String = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Add a char:");
+char Char = char.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(CustomContains(String, Char));
+
+bool CustomContains(string String, char Char)
+{
+    bool Contains = false;
+    for (int i = 0; i < String.Length; i++)
+    {
+        if (Char == String[i])
+        {
+            Contains = true;
+            break;
+        }
+    }
+    return Contains;
+}
---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter a string:");
+string String = Console.ReadLine();
+string Result = CustomReverse(String);
+Console.WriteLine(Result);
+
+string CustomReverse(string String)
+{
+    Result = string.Empty;
+    for(int i = String.Length - 1; i >= 0; i--)
+    {
+        Result += String[i];
+    }
+    return Result;
+}
